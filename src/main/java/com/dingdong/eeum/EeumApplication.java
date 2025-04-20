59bf7671aee664128d895d6c16369207611b1290
boms03 <dvboms@gmail.com>
2025-04-20 23:43:06 +0900

    init

---
+++ src/main/java/com/dingdong/eeum/EeumApplication.java
@@ -0,0 +1,13 @@
+package com.dingdong.eeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dingdong/eeum/EeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingdong.eeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
