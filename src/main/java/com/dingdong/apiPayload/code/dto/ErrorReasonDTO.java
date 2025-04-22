42da515c662a601fc764660429310e52d2bea9ca
boms03 <dvboms@gmail.com>
2025-04-22 22:02:25 +0900

    feat: API 응답 정보를 위한 DTO 클래스 추가
    API 응답 메시지 표준화를 위한 DTO 클래스 구현
    - ReasonDTO: 기본 응답 정보 포맷 정의
    - ErrorReasonDTO: 에러 응답 전용 정보 포맷 정의
    - HTTP 상태 코드, 성공 여부, 코드, 메시지 등 응답 정보 포함
    
    관련: #2

---
+++ src/main/java/com/dingdong/apiPayload/code/dto/ErrorReasonDTO.java
@@ -0,0 +1,21 @@
+package com.dingdong.apiPayload.code.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ErrorReasonDTO {
+
+    private HttpStatus httpStatus;
+
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+
+    public boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+}
---
+++ src/main/java/com/dingdong/apiPayload/code/dto/ReasonDTO.java
@@ -0,0 +1,22 @@
+package com.dingdong.apiPayload.code.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ReasonDTO {
+
+    private final HttpStatus httpStatus;
+
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+
+    public boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+
+}
