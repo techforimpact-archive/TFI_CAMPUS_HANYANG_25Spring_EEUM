c4c3dd91e29b38311e2c3d45a6cda0d0595b8c6f
Yune Cho <fusma159@naver.com>
2025-05-02 01:10:23 +0900

    [FEAT/#21] PlaceRouter에 리뷰 관련 case 추가

--- e-eum/Sources/E_Eum/Utility/Network/Router/PlaceHTTPRequestRouter.swift
+++ e-eum/Sources/E_Eum/Utility/Network/Router/PlaceHTTPRequestRouter.swift
@@ -2,13 +2,17 @@ import Foundation
 
 enum PlaceHTTPRequestRouter {
     case getPlaceDetails(placeID: String)
+    case getPlaceReviews(placeID: String, lastId: String, size: Int, sortBy: String, sortDirection: String)
+    case createPlaceReview(placeID: String)
 }
 
 extension PlaceHTTPRequestRouter: HTTPRequestable {
     var method: HTTPMethod {
         switch self {
-        case .getPlaceDetails:
-                return .get
+        case .getPlaceDetails, .getPlaceReviews:
+            return .get
+        case .createPlaceReview:
+            return .post
         }
     }
     
@@ -24,13 +28,23 @@ extension PlaceHTTPRequestRouter: HTTPRequestable {
         switch self {
         case .getPlaceDetails(let placeID):
             return ["v1", "places", "\(placeID)"]
+        case .getPlaceReviews(let placeID, _, _, _, _), .createPlaceReview(let placeID):
+            return ["v1", "places", "\(placeID)", "reviews"]
         }
     }
     
     var queryItems: [URLQueryItem]? {
         switch self {
-        case .getPlaceDetails:
+        case .getPlaceDetails, .createPlaceReview:
             return nil
+        case .getPlaceReviews(_, let lastID, let size, let sortBy, let sortDirection):
+            var queryItems = [
+                URLQueryItem(name: "lastID", value: lastID),
+                URLQueryItem(name: "size", value: "\(size)"),
+                URLQueryItem(name: "sortBy", value: sortBy),
+                URLQueryItem(name: "sortDirection", value: sortDirection)
+            ]
+            return queryItems
         }
     }
 }
