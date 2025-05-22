f454d533aaab009c5568d83ed09f597d83e9796b
Yune Cho <fusma159@naver.com>
2025-05-22 11:41:16 +0900

    [FEAT/#36] PlaceListView에 무한스크롤 적용

--- e-eum/Sources/E_Eum/Model/UIO/Place/PlaceListUIO.swift
+++ e-eum/Sources/E_Eum/Model/UIO/Place/PlaceListUIO.swift
@@ -3,9 +3,9 @@ import Foundation
 struct PlaceListUIO {
     var places: [PlaceUIO]
     let hasNext: Bool
-    let nextCursor: String
+    let nextCursor: String?
     
-    init(places: [PlaceDTO], hasNext: Bool, nextCursor: String) {
+    init(places: [PlaceDTO], hasNext: Bool, nextCursor: String?) {
         var tmp: [PlaceUIO] = []
         for place in places {
             tmp.append(PlaceUIO(placeDTO: place))
