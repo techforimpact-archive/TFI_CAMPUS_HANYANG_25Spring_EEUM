67732be30f7b2ec5c13f8471733b1260da35a166
Yune Cho <fusma159@naver.com>
2025-05-28 13:21:03 +0900

    [FEAT/#44] KeychainUtility 구현

--- e-eum/Package.swift
+++ e-eum/Package.swift
@@ -14,14 +14,16 @@ let package = Package(
         .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0"),
         .package(url: "https://github.com/skiptools/skip-web", from: "0.1.0"),
         .package(url: "https://github.com/skiptools/skip-kit", from: "0.4.0"),
-        .package(url: "https://github.com/skiptools/skip-device", from: "0.4.0")
+        .package(url: "https://github.com/skiptools/skip-device", from: "0.4.0"),
+        .package(url: "https://github.com/skiptools/skip-keychain", from: "0.3.0")
     ],
     targets: [
         .target(name: "E_Eum", dependencies: [
             .product(name: "SkipUI", package: "skip-ui"),
             .product(name: "SkipWeb", package: "skip-web"),
             .product(name: "SkipKit", package: "skip-kit"),
-            .product(name: "SkipDevice", package: "skip-device")
+            .product(name: "SkipDevice", package: "skip-device"),
+            .product(name: "SkipKeychain", package: "skip-keychain")
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "E_EumTests", dependencies: [
             "E_Eum",
