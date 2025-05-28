4c6da3023c5d6ef3bd50eec9c9ca9865c5a00473
boms03 <dvboms@gmail.com>
2025-05-28 23:39:58 +0900

    feat: role 업데이트 함수
    관련: #22

--- src/main/java/com/dingdong/eeum/repository/UserRepository.java
+++ src/main/java/com/dingdong/eeum/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.dingdong.eeum.repository;
 
+import com.dingdong.eeum.constant.UserRole;
 import com.dingdong.eeum.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +15,9 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
+
+    @Query("{ '_id': ?0 }")
+    @Update("{ '$set': { 'role': ?1, 'updatedAt': ?2 } }")
+    void updateRoleById(String id, UserRole role, LocalDateTime updatedAt);
+
 }
