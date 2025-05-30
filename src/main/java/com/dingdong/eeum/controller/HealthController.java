e650f4d43c663b96cd56ea59252f4bf2a6297e82
boms03 <dvboms@gmail.com>
2025-05-30 13:47:21 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/dingdong/eeum/controller/HealthController.java
+++ src/main/java/com/dingdong/eeum/controller/HealthController.java
@@ -1,12 +1,7 @@
 package com.dingdong.eeum.controller;
 
-import com.dingdong.eeum.apiPayload.exception.response.Response;
-import com.dingdong.eeum.constant.PlaceSearch;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
