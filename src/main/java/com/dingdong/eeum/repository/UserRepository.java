d9f97f332eb4ca3754c318537589911f6f8abe33
boms03 <dvboms@gmail.com>
2025-05-30 13:46:57 +0900

    feat: 활성 유저 조회 메소드

--- src/main/java/com/dingdong/eeum/repository/UserRepository.java
+++ src/main/java/com/dingdong/eeum/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.dingdong.eeum.repository;
 
 import com.dingdong.eeum.constant.UserRole;
+import com.dingdong.eeum.constant.UserStatus;
 import com.dingdong.eeum.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -12,12 +13,11 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndStatus(String email,UserStatus status);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 
     @Query("{ '_id': ?0 }")
     @Update("{ '$set': { 'role': ?1, 'updatedAt': ?2 } }")
     void updateRoleById(String id, UserRole role, LocalDateTime updatedAt);
-
 }
