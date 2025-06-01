369f2947c1f1a3e0deac5e8333a8e65f4db013d3
Yune Cho <fusma159@naver.com>
2025-06-02 01:59:11 +0900

    [FEAT/#48] Router에 내 리뷰 목록 조회, 찜 목록 조회 case 추가

--- e-eum/Sources/E_Eum/Utility/Network/Router/ReviewHTTPRequestRouter.swift
+++ e-eum/Sources/E_Eum/Utility/Network/Router/ReviewHTTPRequestRouter.swift
@@ -4,12 +4,13 @@ enum ReviewHTTPRequestRouter {
     case getReview(token: String, reviewID: String)
     case deleteReview(token: String, reviewID: String)
     case getQuestions(token: String)
+    case myReviews(token: String, cursor: String, size: Int, sortBy: String, sortDirection: String)
 }
 
 extension ReviewHTTPRequestRouter: HTTPRequestable {
     var method: HTTPMethod {
         switch self {
-        case .getReview, .getQuestions:
+        case .getReview, .getQuestions, .myReviews:
             return .get
         case .deleteReview:
             return .delete
@@ -24,12 +25,14 @@ extension ReviewHTTPRequestRouter: HTTPRequestable {
             return ["Authorization": "Bearer \(token)"]
         case .getQuestions(let token):
             return ["Authorization": "Bearer \(token)"]
+        case .myReviews(let token, _, _, _, _):
+            return ["Authorization": "Bearer \(token)"]
         }
     }
     
     var body: Data? {
         switch self {
-        case .getReview, .deleteReview, .getQuestions:
+        case .getReview, .deleteReview, .getQuestions, .myReviews:
             return nil
         }
     }
@@ -52,6 +55,8 @@ extension ReviewHTTPRequestRouter: HTTPRequestable {
             return ["v1", "reviews", "\(reviewID)"]
         case .getQuestions:
             return ["v1", "reviews", "questions"]
+        case .myReviews:
+            return ["v1", "user", "reviews"]
         }
     }
     
@@ -59,6 +64,14 @@ extension ReviewHTTPRequestRouter: HTTPRequestable {
         switch self {
         case .getReview, .deleteReview, .getQuestions:
             return nil
+        case .myReviews(_, let cursor, let size, let sortBy, let sortDirection):
+            let queryItems = [
+                URLQueryItem(name: "cursor", value: cursor),
+                URLQueryItem(name: "size", value: "\(size)"),
+                URLQueryItem(name: "sortBy", value: sortBy),
+                URLQueryItem(name: "sortDirection", value: sortDirection)
+            ]
+            return queryItems
         }
     }
 }
